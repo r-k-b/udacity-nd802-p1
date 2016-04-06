1c6e1cdccd30085c599424963c1105b17889a7fe
Robert K. Bell <robert.k.bell@gmail.com>
2016-04-06 18:35:34 +1000

    feat: add updateMany and removeMany actions to the store
    also, start adding some jsdoc

--- js/appUtils.js
+++ js/appUtils.js
@@ -18,15 +18,55 @@ export const isPresent = curry((itemId, list) => {
 export const eventIdsMatch = curry((eventA, eventB) => (eventA.id === eventB.id));
 
 /**
+ * Update the Store with an Action.
+ *
+ * @typedef {Object} StoreAction
+ * @property {String} action
+ * @property {*} value
+ */
+
+/**
+ * A representation of an Event (e.g., party, meeting, hoedown, etc etc)
+ *
+ * @typedef {Object} Event
+ * @property {String} action
+ * @property {*} value
+ * @property {String} utcStart
+ * @property {String} utcEnd
+ * @property {String} title
+ * @property {String} note
+ * @property {Integer} hostId
+ * @property {String} type
+ * @property {Array<String>} guestNames
+ * @property {String} location
+ */
+
+/**
+ * a → a → a
+ *
  * If the events have the same ID, the second ('newer') one is returned.
  * Otherwise, the first event is returned.
- * 
- * @param oldEvent
- * @param newEvent
+ *
+ * @param {Event} oldEvent
+ * @param {Event} newEvent
+ * @returns {Event}
  */
 export const chooseEvent = (oldEvent, newEvent) =>
   eventIdsMatch(newEvent, oldEvent) ? newEvent : oldEvent;
 
+
+/**
+ * [a] → a → [a]
+ *
+ * If there exists an event in `acc` with the same id as `event`, it will
+ * be replaced.
+ *
+ * If not, `event` will be appended to `acc`.
+ *
+ * @param {Array<Event>} acc
+ * @param {Event} event
+ * @returns {Array<Event>}
+ */
 const listActionUpdate = (acc, event) => {
   if (isPresent(event.id, acc)) {
     return acc.map((oldEvent) => chooseEvent(oldEvent, event))
@@ -34,18 +74,50 @@ const listActionUpdate = (acc, event) => {
   return append(event, acc)
 };
 
-const listActionUpdateMany = (acc, events) => {
-  // for each event in events
-  //    update acc with event
-  // TODO!
-};
+/**
+ * [a] → [a] → [a]
+ *
+ * For each event in `events`:
+ *
+ *    If there exists an event in `acc` with the
+ *    same id as `event`, it will be replaced.
+ *
+ *    If not, `event` will be appended to `acc`.
+ *
+ * @param {Array<Event>} acc
+ * @param {Array<Event>} events
+ * @returns {Array<Event>}
+ */
+const listActionUpdateMany = (acc, events) => reduce(listActionUpdate, acc, events);
 
+
+/**
+ * [a] → a → [a]
+ *
+ * If there exists an event in `acc` with the same id as `event`, it will
+ * be removed.
+ *
+ * @param {Array<Event>} acc
+ * @param {Event} event
+ * @returns {Array<Event>}
+ */
 const listActionRemove = (acc, event) => 
   filter(compose(not, eventIdsMatch(event)), acc);
 
-const listActionRemoveMany = (acc, events) => {
-  // TODO!
-};
+
+/**
+ * [a] → [a] → [a]
+ *
+ * For each event in `events`:
+ *
+ *    If there exists an event in `acc` with the
+ *    same id as `event`, it will be removed.
+ *
+ * @param {Array<Event>} acc
+ * @param {Array<Event>} events
+ * @returns {Array<Event>}
+ */
+const listActionRemoveMany = (acc, events) => reduce(listActionRemove, acc, events);
 
 /*
  * update
