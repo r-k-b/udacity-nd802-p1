e4bcfbb94c364936407807ba32a673dbd4cc00d0
Robert K. Bell <robert.k.bell@gmail.com>
2016-04-26 21:07:28 +1000

    chore: add build task for prefixed & minified css

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,8 +5,11 @@ import {merge} from 'ramda';
 import gutil from 'gulp-util';
 import watchify from 'watchify';
 import buffer from 'vinyl-buffer';
+import postcss from 'gulp-postcss';
+import cssnano from 'gulp-cssnano';
 import browserify from 'browserify';
 import BrowserSync from 'browser-sync';
+import autoprefixer from 'autoprefixer';
 import sourcemaps from 'gulp-sourcemaps';
 import source from 'vinyl-source-stream';
 
@@ -57,12 +60,26 @@ gulp.task('sync', () =>
   })
 );
 
+
 gulp.watch('./*.html').on('change', browserSync.reload);
+
 
 gulp.task('watchTests', () => {
   gulp.watch('./spec/*.js').on('change', () => {
     // todo: `npm run test` (tape) from here (how?)
   });
 });
+
+
+// todo: add Watch support
+// todo: update links to the generated css
+gulp.task('minifyCss', () => gulp.src('./css/*.css')
+  .pipe(sourcemaps.init())
+  .pipe(postcss([ autoprefixer() ]))
+  .pipe(cssnano())
+  .pipe(sourcemaps.write('.'))
+  .pipe(gulp.dest('./dist/css'))
+);
+
 
 gulp.task('default', ['sync', 'watchify']);
